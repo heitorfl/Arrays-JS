56bf1521b6f157e332e443c4ef29f70e86a5d187
Heitor Lima <141742426+heitorfl@users.noreply.github.com>
2024-10-10 14:09:54 -0300

    Update script.js

--- Sorteio/script.js
+++ Sorteio/script.js
@@ -5,13 +5,8 @@ let i = 0;
 
 for(i=0;i<5;i++){
     a[i] = parseInt(prompt("Digite um número"))
-
-}
-
-for(i=0;i<5;i++){
     b[i] = parseInt(Math.random () * 5)
 }
-
 for(i=0;i<5;i++){
     c[i] = a[i] * b[i]
     console.log(`c = ${c[i]}`)
